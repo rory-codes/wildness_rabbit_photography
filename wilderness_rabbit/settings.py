047973e45464d98860cf1d4d83563e6868e4014d
rory-codes <rory.thompson@outlook.com>
2026-01-12 14:03:23 +0000

    Prod-safe settings (SECRET_KEY from env, no collectstatic flag in code)

--- wilderness_rabbit/settings.py
+++ wilderness_rabbit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v7vs^p%ka32+o##&-#)+dl*-&6ffc-%9og()u*aryhdn^mgpyh'
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
