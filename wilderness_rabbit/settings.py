3b10f811c4a92bb2bbcbe5c2fd3b47a618cee4d4
rory-codes <rory.thompson@outlook.com>
2026-01-13 14:11:11 +0000

    Fix ROOT_URLCONF/WSGI to wilderness_rabbit.*

--- wilderness_rabbit/settings.py
+++ wilderness_rabbit/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
+SECRET_KEY = os.getenv("SECRET_KEY", "generate-a-strong-random-string")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -68,7 +68,7 @@ MIDDLEWARE = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = "wilderness_rabbit_photography.urls"
+ROOT_URLCONF = "wilderness_rabbit.urls"
  
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]  
