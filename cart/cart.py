f69711e4303374879715575743df96d7ddd16eed
rory-codes <rory.thompson@outlook.com>
2026-01-16 15:44:53 +0000

    Implement add and remove methods for cart functionality

--- cart/cart.py
+++ cart/cart.py
@@ -11,3 +11,17 @@ class Cart:
             cart = {}
             self.session[CART_SESSION_KEY] = cart
         self.cart = cart
+
+    def add(self, variant_id, price, qty=1):
+        key = str(variant_id)
+        line = self.cart.get(key, {"qty": 0, "price": str(price)})
+        line["qty"] = int(line["qty"]) + int(qty)
+        line["price"] = str(price)
+        self.cart[key] = line
+        self.session.modified = True
+
+    def remove(self, variant_id):
+        key = str(variant_id)
+        if key in self.cart:
+            del self.cart[key]
+            self.session.modified = True
