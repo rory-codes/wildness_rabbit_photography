4c7e107747e43d853562fcb200fd14be92eccd16
rory-codes <rory.thompson@outlook.com>
2026-01-19 14:37:31 +0000

    Register Order and OrderItem models in the admin interface

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "paid", "stripe_session_id", "created_at")
+    list_filter = ("paid",)
+    search_fields = ("email", "stripe_session_id")
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "variant", "qty", "price")
+    search_fields = ("order__id", "variant__name", "variant__photo__title")
