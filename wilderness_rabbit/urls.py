40b42aa43393956858895e069f55cebbd89e715e
rory-codes <rory.thompson@outlook.com>
2026-01-23 14:58:26 +0000

    Refactor login and signup templates for improved styling and consistency; add redirect for user profile URL

--- wilderness_rabbit/urls.py
+++ wilderness_rabbit/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from catalog import views as catalog_views
 
 urlpatterns = [
     #path("", catalog_views.home, name="home"),
     path("", include(("catalog.urls", "catalog"), namespace="catalog")),
+    path("accounts/profile/", RedirectView.as_view(url="/", permanent=False)),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path("cart/", include(("cart.urls", "cart"), namespace="cart")),
