43a75210f0ecf25edeb94fe3f97a3a835551d4d0
rory-codes <rory.thompson@outlook.com>
2026-03-06 14:14:53 +0000

    Update urls.py for testimonials to include crud functionality

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = "testimonials"
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add/", views.create_testimonial, name="add"),
+    path("edit/<int:pk>/", views.edit_testimonial, name="edit"),
+    path("delete/<int:pk>/", views.delete_testimonial, name="delete"),
+    ]
