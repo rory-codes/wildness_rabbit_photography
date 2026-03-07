71b5d476af00e05c1691ea5afcd11f44c4f21515
rory-codes <rory.thompson@outlook.com>
2026-03-07 16:14:18 +0000

    Remove webhook URL from checkout urlpatterns

--- checkout/urls.py
+++ checkout/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("start/", views.start_checkout, name="start"),
     path("success/", views.success, name="success"),
     path("cancel/", views.cancel, name="cancel"),
-    path("webhook/", webhooks.stripe_webhook, name="webhook"),
 ]
